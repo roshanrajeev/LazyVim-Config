cc9cbfef1b228ef957776497225c363bf6f49d05
Roshan R <roshan@Macbook-Pro.local>
2023-07-26 17:29:37 +0530

    Keymap - Toggle neo-tree/editor Focus

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>o", function()
+  if vim.bo.filetype == "neo-tree" then
+    vim.cmd.wincmd("p")
+  else
+    vim.cmd.Neotree("focus")
+  end
+end)
